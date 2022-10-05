dabaa5d58fb9a0babb049110288d12285157786a
BreezeRzZz <84554550+BreezeRzZz@users.noreply.github.com>
2022-10-05 20:20:55 +0800

    Add files via upload

--- Homework7/Assignment7/Material.hpp
+++ Homework7/Assignment7/Material.hpp
@@ -7,173 +7,255 @@
 
 #include "Vector.hpp"
 
-enum MaterialType { DIFFUSE};
+enum MaterialType { DIFFUSE, MICROFACET };
 
-class Material{
+class Material {
 private:
 
-    // Compute reflection direction
-    Vector3f reflect(const Vector3f &I, const Vector3f &N) const
-    {
-        return I - 2 * dotProduct(I, N) * N;
-    }
+	// Compute reflection direction
+	Vector3f reflect(const Vector3f& I, const Vector3f& N) const
+	{
+		return I - 2 * dotProduct(I, N) * N;
+	}
 
-    // Compute refraction direction using Snell's law
-    //
-    // We need to handle with care the two possible situations:
-    //
-    //    - When the ray is inside the object
-    //
-    //    - When the ray is outside.
-    //
-    // If the ray is outside, you need to make cosi positive cosi = -N.I
-    //
-    // If the ray is inside, you need to invert the refractive indices and negate the normal N
-    Vector3f refract(const Vector3f &I, const Vector3f &N, const float &ior) const
-    {
-        float cosi = clamp(-1, 1, dotProduct(I, N));
-        float etai = 1, etat = ior;
-        Vector3f n = N;
-        if (cosi < 0) { cosi = -cosi; } else { std::swap(etai, etat); n= -N; }
-        float eta = etai / etat;
-        float k = 1 - eta * eta * (1 - cosi * cosi);
-        return k < 0 ? 0 : eta * I + (eta * cosi - sqrtf(k)) * n;
-    }
+	// Compute refraction direction using Snell's law
+	//
+	// We need to handle with care the two possible situations:
+	//
+	//    - When the ray is inside the object
+	//
+	//    - When the ray is outside.
+	//
+	// If the ray is outside, you need to make cosi positive cosi = -N.I
+	//
+	// If the ray is inside, you need to invert the refractive indices and negate the normal N
+	Vector3f refract(const Vector3f& I, const Vector3f& N, const float& ior) const
+	{
+		float cosi = clamp(-1, 1, dotProduct(I, N));
+		float etai = 1, etat = ior;
+		Vector3f n = N;
+		if (cosi < 0) { cosi = -cosi; }
+		else { std::swap(etai, etat); n = -N; }
+		float eta = etai / etat;
+		float k = 1 - eta * eta * (1 - cosi * cosi);
+		return k < 0 ? 0 : eta * I + (eta * cosi - sqrtf(k)) * n;
+	}
 
-    // Compute Fresnel equation
-    //
-    // \param I is the incident view direction
-    //
-    // \param N is the normal at the intersection point
-    //
-    // \param ior is the material refractive index
-    //
-    // \param[out] kr is the amount of light reflected
-    void fresnel(const Vector3f &I, const Vector3f &N, const float &ior, float &kr) const
-    {
-        float cosi = clamp(-1, 1, dotProduct(I, N));
-        float etai = 1, etat = ior;
-        if (cosi > 0) {  std::swap(etai, etat); }
-        // Compute sini using Snell's law
-        float sint = etai / etat * sqrtf(std::max(0.f, 1 - cosi * cosi));
-        // Total internal reflection
-        if (sint >= 1) {
-            kr = 1;
-        }
-        else {
-            float cost = sqrtf(std::max(0.f, 1 - sint * sint));
-            cosi = fabsf(cosi);
-            float Rs = ((etat * cosi) - (etai * cost)) / ((etat * cosi) + (etai * cost));
-            float Rp = ((etai * cosi) - (etat * cost)) / ((etai * cosi) + (etat * cost));
-            kr = (Rs * Rs + Rp * Rp) / 2;
-        }
-        // As a consequence of the conservation of energy, transmittance is given by:
-        // kt = 1 - kr;
-    }
+	// Compute Fresnel equation
+	//
+	// \param I is the incident view direction
+	//
+	// \param N is the normal at the intersection point
+	//
+	// \param ior is the material refractive index
+	//
+	// \param[out] kr is the amount of light reflected
+	void fresnel(const Vector3f& I, const Vector3f& N, const float& ior, float& kr) const
+	{
+		float cosi = clamp(-1, 1, dotProduct(I, N));
+		float etai = 1, etat = ior;
+		if (cosi > 0) { std::swap(etai, etat); }
+		// Compute sini using Snell's law
+		float sint = etai / etat * sqrtf(std::max(0.f, 1 - cosi * cosi));
+		// Total internal reflection
+		if (sint >= 1) {
+			kr = 1;
+		}
+		else {
+			float cost = sqrtf(std::max(0.f, 1 - sint * sint));
+			cosi = fabsf(cosi);
+			float Rs = ((etat * cosi) - (etai * cost)) / ((etat * cosi) + (etai * cost));
+			float Rp = ((etai * cosi) - (etat * cost)) / ((etai * cosi) + (etat * cost));
+			kr = (Rs * Rs + Rp * Rp) / 2;
+		}
+		// As a consequence of the conservation of energy, transmittance is given by:
+		// kt = 1 - kr;
+	}
 
-    Vector3f toWorld(const Vector3f &a, const Vector3f &N){
-        Vector3f B, C;
-        if (std::fabs(N.x) > std::fabs(N.y)){
-            float invLen = 1.0f / std::sqrt(N.x * N.x + N.z * N.z);
-            C = Vector3f(N.z * invLen, 0.0f, -N.x *invLen);
-        }
-        else {
-            float invLen = 1.0f / std::sqrt(N.y * N.y + N.z * N.z);
-            C = Vector3f(0.0f, N.z * invLen, -N.y *invLen);
-        }
-        B = crossProduct(C, N);
-        return a.x * B + a.y * C + a.z * N;
-    }
+	Vector3f toWorld(const Vector3f& a, const Vector3f& N) {
+		Vector3f B, C;
+		if (std::fabs(N.x) > std::fabs(N.y)) {
+			float invLen = 1.0f / std::sqrt(N.x * N.x + N.z * N.z);
+			C = Vector3f(N.z * invLen, 0.0f, -N.x * invLen);
+		}
+		else {
+			float invLen = 1.0f / std::sqrt(N.y * N.y + N.z * N.z);
+			C = Vector3f(0.0f, N.z * invLen, -N.y * invLen);
+		}
+		B = crossProduct(C, N);
+		return a.x * B + a.y * C + a.z * N;
+	}
+
+	//法线分布函数(GGX)
+	float DistributionGGX(Vector3f n, Vector3f h, float roughness)
+	{
+		float a = pow(roughness, 4);
+		float NdotH = std::max(dotProduct(n, h), 0.0f);
+		float b = (NdotH * NdotH) * (a - 1) + 1;
+		return a / (M_PI * b * b);
+	}
+
+	//阴影遮挡函数(UE4)
+	float GeometryGGX(Vector3f n, Vector3f v, float roughness)
+	{
+		float NdotV = std::max(dotProduct(n, v), 0.0f);
+		float k = (roughness + 1) * (roughness + 1) / 8;
+		return NdotV / (NdotV * (1 - k) + k);
+	}
+
+	float GeometryFunc(Vector3f n, Vector3f v, Vector3f l, float roughness)
+	{
+		return GeometryGGX(n, v, roughness) * GeometryGGX(n, l, roughness);
+	}
 
 public:
-    MaterialType m_type;
-    //Vector3f m_color;
-    Vector3f m_emission;
-    float ior;
-    Vector3f Kd, Ks;
-    float specularExponent;
-    //Texture tex;
+	MaterialType m_type;
+	//Vector3f m_color;
+	Vector3f m_emission;
+	float ior;
+	Vector3f Kd, Ks;
+	float specularExponent;
+	//Texture tex;
 
-    inline Material(MaterialType t=DIFFUSE, Vector3f e=Vector3f(0,0,0));
-    inline MaterialType getType();
-    //inline Vector3f getColor();
-    inline Vector3f getColorAt(double u, double v);
-    inline Vector3f getEmission();
-    inline bool hasEmission();
+	inline Material(MaterialType t = DIFFUSE, Vector3f e = Vector3f(0, 0, 0));
+	inline MaterialType getType();
+	//inline Vector3f getColor();
+	inline Vector3f getColorAt(double u, double v);
+	inline Vector3f getEmission();
+	inline bool hasEmission();
 
-    // sample a ray by Material properties
-    inline Vector3f sample(const Vector3f &wi, const Vector3f &N);
-    // given a ray, calculate the PdF of this ray
-    inline float pdf(const Vector3f &wi, const Vector3f &wo, const Vector3f &N);
-    // given a ray, calculate the contribution of this ray
-    inline Vector3f eval(const Vector3f &wi, const Vector3f &wo, const Vector3f &N);
+	// sample a ray by Material properties
+	inline Vector3f sample(const Vector3f& wi, const Vector3f& N);
+	// given a ray, calculate the PdF of this ray
+	inline float pdf(const Vector3f& wi, const Vector3f& wo, const Vector3f& N);
+	// given a ray, calculate the contribution of this ray
+	inline Vector3f eval(const Vector3f& wi, const Vector3f& wo, const Vector3f& N);
 
 };
 
-Material::Material(MaterialType t, Vector3f e){
-    m_type = t;
-    //m_color = c;
-    m_emission = e;
+Material::Material(MaterialType t, Vector3f e) {
+	m_type = t;
+	//m_color = c;
+	m_emission = e;
 }
 
-MaterialType Material::getType(){return m_type;}
+MaterialType Material::getType() { return m_type; }
 ///Vector3f Material::getColor(){return m_color;}
-Vector3f Material::getEmission() {return m_emission;}
+Vector3f Material::getEmission() { return m_emission; }
 bool Material::hasEmission() {
-    if (m_emission.norm() > EPSILON) return true;
-    else return false;
+	if (m_emission.norm() > EPSILON) return true;
+	else return false;
 }
 
 Vector3f Material::getColorAt(double u, double v) {
-    return Vector3f();
+	return Vector3f();
 }
 
 
-Vector3f Material::sample(const Vector3f &wi, const Vector3f &N){
-    switch(m_type){
-        case DIFFUSE:
-        {
-            // uniform sample on the hemisphere
-            float x_1 = get_random_float(), x_2 = get_random_float();
-            float z = std::fabs(1.0f - 2.0f * x_1);
-            float r = std::sqrt(1.0f - z * z), phi = 2 * M_PI * x_2;
-            Vector3f localRay(r*std::cos(phi), r*std::sin(phi), z);
-            return toWorld(localRay, N);
-            
-            break;
-        }
-    }
+Vector3f Material::sample(const Vector3f& wi, const Vector3f& N) {
+	switch (m_type) {
+	case DIFFUSE:
+	{
+		// uniform sample on the hemisphere
+		float x_1 = get_random_float(), x_2 = get_random_float();
+		float z = std::fabs(1.0f - 2.0f * x_1);
+		float r = std::sqrt(1.0f - z * z), phi = 2 * M_PI * x_2;
+		Vector3f localRay(r * std::cos(phi), r * std::sin(phi), z);
+		return toWorld(localRay, N);
+
+		break;
+	}
+	case MICROFACET:
+	{
+		// uniform sample on the hemisphere
+		float x_1 = get_random_float(), x_2 = get_random_float();
+		float z = std::fabs(1.0f - 2.0f * x_1);
+		float r = std::sqrt(1.0f - z * z), phi = 2 * M_PI * x_2;
+		Vector3f localRay(r * std::cos(phi), r * std::sin(phi), z);
+		return toWorld(localRay, N);
+
+		break;
+	}
+	}
 }
 
-float Material::pdf(const Vector3f &wi, const Vector3f &wo, const Vector3f &N){
-    switch(m_type){
-        case DIFFUSE:
-        {
-            // uniform sample probability 1 / (2 * PI)
-            if (dotProduct(wo, N) > 0.0f)
-                return 0.5f / M_PI;
-            else
-                return 0.0f;
-            break;
-        }
-    }
+float Material::pdf(const Vector3f& wi, const Vector3f& wo, const Vector3f& N) {
+	switch (m_type) {
+	case DIFFUSE:
+	{
+		// uniform sample probability 1 / (2 * PI)
+		if (dotProduct(wo, N) > 0.0f)
+			return 0.5f / M_PI;
+		else
+			return 0.0f;
+		break;
+	}
+	case MICROFACET:
+	{
+		// uniform sample probability 1 / (2 * PI)
+		if (dotProduct(wo, N) > 0.0f)
+			return 0.5f / M_PI;
+		else
+			return 0.0f;
+		break;
+	}
+	}
 }
 
-Vector3f Material::eval(const Vector3f &wi, const Vector3f &wo, const Vector3f &N){
-    switch(m_type){
-        case DIFFUSE:
-        {
-            // calculate the contribution of diffuse   model
-            float cosalpha = dotProduct(N, wo);
-            if (cosalpha > 0.0f) {
-                Vector3f diffuse = Kd / M_PI;
-                return diffuse;
-            }
-            else
-                return Vector3f(0.0f);
-            break;
-        }
-    }
+Vector3f Material::eval(const Vector3f& wi, const Vector3f& wo, const Vector3f& N) {
+	switch (m_type) {
+	case DIFFUSE:
+	{
+		// calculate the contribution of diffuse   model
+		float cosalpha = dotProduct(N, wo);
+		if (cosalpha > 0.0f) {
+			Vector3f diffuse = Kd / M_PI;
+			return diffuse;
+		}
+		else
+			return Vector3f(0.0f);
+		break;
+	}
+	case MICROFACET:
+	{
+		float cosalpha = dotProduct(N, wo);
+		if (cosalpha > 0.0f)
+		{
+			float roughness = 0.8;
+			//半程向量,这里要用-wi，因为wi是入射方向
+			Vector3f h = (-wi + wo).normalized();
+
+			//法线分布
+			float D = DistributionGGX(N, h, roughness);
+
+			//阴影遮蔽
+			float G = GeometryFunc(N, wo, -wi, roughness);
+
+			//折射率
+			float ior = 1.5;
+			float F;
+			//菲涅尔项
+			fresnel(wi, N, ior, F);
+
+			//镜面反射BRDF
+			float m = 4 * std::max(dotProduct(N, -wi) * dotProduct(N, -wi), 0.0001f);
+			float specular = D * G * F / m;
+
+			//漫反射BRDF
+			//漫反射反射率
+			float rho = 1.0f;
+			float diffuse = rho / M_PI;
+
+			float ks = F;
+			float kd = 1 - F;
+
+			return Kd * diffuse * kd + Ks * ks * specular;
+		}
+		else
+			return Vector3f(0.0f);
+		break;
+	}
+	}
 }
 
 #endif //RAYTRACING_MATERIAL_H
--- Homework7/Assignment7/main.cpp
+++ Homework7/Assignment7/main.cpp
@@ -13,44 +13,51 @@
 int main(int argc, char** argv)
 {
 
-    // Change the definition here to change resolution
-    Scene scene(784, 784);
+	// Change the definition here to change resolution
+	Scene scene(784, 784);
 
-    Material* red = new Material(DIFFUSE, Vector3f(0.0f));
-    red->Kd = Vector3f(0.63f, 0.065f, 0.05f);
-    Material* green = new Material(DIFFUSE, Vector3f(0.0f));
-    green->Kd = Vector3f(0.14f, 0.45f, 0.091f);
-    Material* white = new Material(DIFFUSE, Vector3f(0.0f));
-    white->Kd = Vector3f(0.725f, 0.71f, 0.68f);
-    Material* light = new Material(DIFFUSE, (8.0f * Vector3f(0.747f+0.058f, 0.747f+0.258f, 0.747f) + 15.6f * Vector3f(0.740f+0.287f,0.740f+0.160f,0.740f) + 18.4f *Vector3f(0.737f+0.642f,0.737f+0.159f,0.737f)));
-    light->Kd = Vector3f(0.65f);
+	Material* Microfacet = new Material(MICROFACET, Vector3f(0.0f));
+	Microfacet->Kd = Vector3f(0.5, 0.5, 0.5);
+	Microfacet->Ks = Vector3f(0.8, 0.8, 0.8);
 
-    MeshTriangle floor("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/floor.obj", white);
-    MeshTriangle shortbox("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/shortbox.obj", white);
-    MeshTriangle tallbox("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/tallbox.obj", white);
-    MeshTriangle left("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/left.obj", red);
-    MeshTriangle right("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/right.obj", green);
-    MeshTriangle light_("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/light.obj", light);
+	Material* red = new Material(DIFFUSE, Vector3f(0.0f));
+	red->Kd = Vector3f(0.63f, 0.065f, 0.05f);
+	Material* green = new Material(DIFFUSE, Vector3f(0.0f));
+	green->Kd = Vector3f(0.14f, 0.45f, 0.091f);
+	Material* white = new Material(DIFFUSE, Vector3f(0.0f));
+	white->Kd = Vector3f(0.725f, 0.71f, 0.68f);
+	Material* light = new Material(DIFFUSE, (8.0f * Vector3f(0.747f + 0.058f, 0.747f + 0.258f, 0.747f) + 15.6f * Vector3f(0.740f + 0.287f, 0.740f + 0.160f, 0.740f) + 18.4f * Vector3f(0.737f + 0.642f, 0.737f + 0.159f, 0.737f)));
+	light->Kd = Vector3f(0.65f);
 
-    scene.Add(&floor);
-    scene.Add(&shortbox);
-    scene.Add(&tallbox);
-    scene.Add(&left);
-    scene.Add(&right);
-    scene.Add(&light_);
+	MeshTriangle floor("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/floor.obj", white);
+	//MeshTriangle shortbox("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/shortbox.obj", white);
+	//MeshTriangle tallbox("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/tallbox.obj", white);
+	MeshTriangle left("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/left.obj", red);
+	MeshTriangle right("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/right.obj", green);
+	MeshTriangle light_("D:/StudyRelated/Games101/GAMES101_Homework_S2021/Homework7/Assignment7/models/cornellbox/light.obj", light);
 
-    scene.buildBVH();
+	Sphere sphere(Vector3f(300, 100, 300), 100, Microfacet);
 
-    Renderer r;
+	scene.Add(&floor);
+	scene.Add(&sphere);
+	//scene.Add(&shortbox);
+	//scene.Add(&tallbox);
+	scene.Add(&left);
+	scene.Add(&right);
+	scene.Add(&light_);
 
-    auto start = std::chrono::system_clock::now();
-    r.Render(scene);
-    auto stop = std::chrono::system_clock::now();
+	scene.buildBVH();
 
-    std::cout << "Render complete: \n";
-    std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::hours>(stop - start).count() << " hours\n";
-    std::cout << "          : " << std::chrono::duration_cast<std::chrono::minutes>(stop - start).count() << " minutes\n";
-    std::cout << "          : " << std::chrono::duration_cast<std::chrono::seconds>(stop - start).count() << " seconds\n";
+	Renderer r;
 
-    return 0;
+	auto start = std::chrono::system_clock::now();
+	r.Render(scene);
+	auto stop = std::chrono::system_clock::now();
+
+	std::cout << "Render complete: \n";
+	std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::hours>(stop - start).count() << " hours\n";
+	std::cout << "          : " << std::chrono::duration_cast<std::chrono::minutes>(stop - start).count() << " minutes\n";
+	std::cout << "          : " << std::chrono::duration_cast<std::chrono::seconds>(stop - start).count() << " seconds\n";
+
+	return 0;
 }
